19339a83b8b2976ba0b4bbf8fe1df7e90f2cab63
Julia Merk <you@example.com>
2021-11-07 22:55:29 +0100

    Enums hinzugefügt

---
+++ JuristicGame/Enums/EnumRoleNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JuristicGame
+{
+    public enum EnumRoleNames
+    {
+        ADMIN = 1,
+        DEFAULT_USER = 2
+    }
+}
